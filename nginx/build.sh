785b99fdd18f66c40026f306e72773b40b7e234a
sm8949752-ux <sm8949752@gmail.com>
2026-01-12 18:20:31 +0000

    Add Nginx configuration files and enhance build scripts
    - Introduced new Nginx configuration files: mime.types, fastcgi_params, and server block in nginx.conf.
    - Updated Dockerfile to copy new configuration files into the image.
    - Enhanced build.sh to support hardened compilation flags and improved argument handling.
    - Created modules.def and profiles.def for better module management and build profiles.

--- nginx/build.sh
+++ nginx/build.sh
@@ -25,39 +25,50 @@ echo 'nginx:x:101:' >> /rootfs/etc/group
 ZLIB_CFLAGS=$(pkg-config --cflags zlib)
 ZLIB_LDFLAGS=$(pkg-config --libs zlib)
 
-CC_OPT="-Wno-error $ZLIB_CFLAGS"
-LD_OPT="$ZLIB_LDFLAGS"
+HARDENING_CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIC"
+HARDENING_LDFLAGS="-Wl,-z,relro -Wl,-z,now -fPIC"
 
-CONFIG_ARGS="--prefix=/etc/nginx \
-    --sbin-path=/usr/sbin/nginx \
-    --modules-path=/usr/lib/nginx/modules \
-    --conf-path=/etc/nginx/nginx.conf \
-    --error-log-path=/var/log/nginx/error.log \
-    --http-log-path=/var/log/nginx/access.log \
-    --pid-path=/var/run/nginx.pid \
-    --lock-path=/var/run/nginx.lock \
-    --http-client-body-temp-path=/var/cache/nginx/client_temp \
-    --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
-    --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
-    --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
-    --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
-    --user=nginx --group=nginx \
-    --with-compat --with-file-aio --with-threads \
-    --with-http_ssl_module --with-http_v2_module \
-    --with-http_realip_module --with-http_gunzip_module \
-    --with-http_gzip_static_module --with-http_slice_module \
-    --with-http_stub_status_module \
-    --with-stream=dynamic \
-    --with-stream_ssl_module \
-    --with-stream_realip_module \
-    --with-cc-opt=${CC_OPT} \
-    --with-ld-opt=${LD_OPT}"
+CC_OPT="-Wno-error $ZLIB_CFLAGS $HARDENING_CFLAGS"
+LD_OPT="$ZLIB_LDFLAGS $HARDENING_LDFLAGS"
+
+CONFIG_ARGS=(
+    "--prefix=/etc/nginx"
+    "--sbin-path=/usr/sbin/nginx"
+    "--modules-path=/usr/lib/nginx/modules"
+    "--conf-path=/etc/nginx/nginx.conf"
+    "--error-log-path=/var/log/nginx/error.log"
+    "--http-log-path=/var/log/nginx/access.log"
+    "--pid-path=/var/run/nginx.pid"
+    "--lock-path=/var/run/nginx.lock"
+    "--http-client-body-temp-path=/var/cache/nginx/client_temp"
+    "--http-proxy-temp-path=/var/cache/nginx/proxy_temp"
+    "--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp"
+    "--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp"
+    "--http-scgi-temp-path=/var/cache/nginx/scgi_temp"
+    "--user=nginx"
+    "--group=nginx"
+    "--with-compat"
+    "--with-file-aio"
+    "--with-threads"
+    "--with-http_ssl_module"
+    "--with-http_v2_module"
+    "--with-http_realip_module"
+    "--with-http_gunzip_module"
+    "--with-http_gzip_static_module"
+    "--with-http_slice_module"
+    "--with-http_stub_status_module"
+    "--with-stream=dynamic"
+    "--with-stream_ssl_module"
+    "--with-stream_realip_module"
+    "--with-cc-opt=${CC_OPT}"
+    "--with-ld-opt=${LD_OPT}"
+)
 
 if [ -n "$NJS_VERSION" ]; then
     wget -qO njs.tar.gz "https://github.com/nginx/njs/archive/refs/tags/${NJS_VERSION}.tar.gz"
     check_hash "njs.tar.gz" "$NJS_SHA256"
     tar -xzf njs.tar.gz
-    CONFIG_ARGS="$CONFIG_ARGS --add-dynamic-module=../njs-${NJS_VERSION}/nginx"
+    CONFIG_ARGS+=("--add-dynamic-module=../njs-${NJS_VERSION}/nginx")
 fi
 
 wget -qO nginx.tar.gz "http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
@@ -65,7 +76,7 @@ check_hash "nginx.tar.gz" "$NGINX_SHA256"
 tar -xzf nginx.tar.gz
 cd nginx-${NGINX_VERSION}
 
-./configure $CONFIG_ARGS || { echo "Configure Failed"; cat objs/autoconf.err; exit 1; }
+./configure "${CONFIG_ARGS[@]}" || { echo "Configure Failed"; cat objs/autoconf.err; exit 1; }
 
 make -j"$(nproc)"
 make install DESTDIR=/rootfs
